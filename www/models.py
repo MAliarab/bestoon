6a659f05a07f16ec473ccccd18dc66c1ac0f66b8
mahmood <arab6320@gmail.com>
2017-10-20 08:03:33 +0330

    complete Income & Expense

--- www/models.py
+++ www/models.py
@@ -2,5 +2,22 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
+from datetime import datetime
+
 
 # Create your models here.
+
+class Expense (models.Model):
+
+    text = models.CharField(max_length=255)
+    date = models.DateField()
+    amount = models.BigIntegerField()
+    user = models.ForeignKey(User)
+
+class Income (models.Model):
+
+    user = models.ForeignKey(User)
+    date = models.DateField()
+    text = models.CharField(max_length=255)
+    amount  = models.BigIntegerField()
